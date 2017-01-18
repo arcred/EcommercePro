f039f15e843beeabcd7af55e890c880e7e5a8f39
arcred <archith.manne@gmail.com>
2017-01-18 14:49:19 +0530

    updated server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ var bodyParser=require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.listen(3000);
+app.listen(8080);
 
-console.log("server running on port 3000");
+console.log("server running on port 8080");
 
 var session = require('express-session');
 
