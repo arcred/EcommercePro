b096fa787e8e9be13846399e139030f4885ddaa6
Archith <archith.manne@gmail.com>
2017-01-20 12:50:48 +0530

    Update controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -914,9 +914,10 @@ myApp.controller('loginController',['$scope','$http', '$localStorage', '$session
 							loginStatus="true";
 							email=response.data[2][0].emailid;
                             a.userName=response.data[2][0].firstname;
+							f.init();
 							e.path('/');
 							
-							f.init();
+							
                             
                             
                             
